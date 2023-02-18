01b369725e218b293f803e5707b47465fe085626
Shivanipalla <117855716+Shivanipalla@users.noreply.github.com>
2023-02-18 21:07:23 +0100

    Add files via upload

---
+++ python_ex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+n=5;
+for i in range(0,n):
+    k=ord('E');
+    for j in range(0,i+1):
+        print(chr(k-j),
+                   end=" ");
+    print(" ");
+        
+    
+
+
+# In[ ]:
+
+
+
+
